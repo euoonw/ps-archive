e21f2caf2362aea85a774bdc2a55f311676f647f
강은우 <80903685+euoonw@users.noreply.github.com>
2026-02-27 14:51:24 +0900

    [level 2] Title: 예상 대진표, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,12 @@
+function solution(n,a,b)
+{
+    let answer = 1;
+    while(true){
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
+        if(a===b) break;
+        answer++;
+    }
+
+    return answer;
+}
