72433abcf0c5c0d04e85328f155dbac4c23c1f50
강은우 <80903685+euoonw@users.noreply.github.com>
2026-05-10 23:08:10 +0900

    [level 2] Title: 모음 사전, Time: 0.98 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,24 +1,58 @@
-function solution(word) {
-  const s = ["", "A", "E", "I", "O", "U"];
-  const dic = [];
-  for (let i1 = 1; i1 < s.length; i1++) {
-    const t1 = s[i1];
-    dic.push(t1);
-    for (let i2 = 1; i2 < s.length; i2++) {
-      const t2 = t1 + s[i2];
-      dic.push(t2);
-      for (let i3 = 1; i3 < s.length; i3++) {
-        const t3 = t2 + s[i3];
-        dic.push(t3);
-        for (let i4 = 1; i4 < s.length; i4++) {
-          const t4 = t3 + s[i4];
-          dic.push(t4);
-          for (let i5 = 1; i5 < s.length; i5++) {
-            dic.push(t4 + s[i5]);
-          }
-        }
-      }
+// function solution(word) {
+//   const s = ["", "A", "E", "I", "O", "U"];
+//   const dic = [];
+//   for (let i1 = 1; i1 < s.length; i1++) {
+//     const t1 = s[i1];
+//     dic.push(t1);
+//     for (let i2 = 1; i2 < s.length; i2++) {
+//       const t2 = t1 + s[i2];
+//       dic.push(t2);
+//       for (let i3 = 1; i3 < s.length; i3++) {
+//         const t3 = t2 + s[i3];
+//         dic.push(t3);
+//         for (let i4 = 1; i4 < s.length; i4++) {
+//           const t4 = t3 + s[i4];
+//           dic.push(t4);
+//           for (let i5 = 1; i5 < s.length; i5++) {
+//             dic.push(t4 + s[i5]);
+//           }
+//         }
+//       }
+//     }
+//   }
+//   return dic.indexOf(word) + 1;
+// }
+
+function getPermutations(words, current, result) {
+    if (current !== "") result.push(current);
+    if (current.length === 5) return;
+    
+    for (let i=0; i<words.length; i++) {
+        const next_current = current + words[i];
+        getPermutations(words, next_current, result);
     }
-  }
-  return dic.indexOf(word) + 1;
 }
+
+function solution(word) {
+    const result = [];
+    const words = ["A", "E", "I", "O", "U"];
+    getPermutations(words, "", result);
+    // console.log(result);
+    return result.indexOf(word) + 1;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
