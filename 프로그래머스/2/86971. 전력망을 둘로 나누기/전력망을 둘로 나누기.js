9d52ef815957a4fbd77de01e492c5b115ee365a2
강은우 <80903685+euoonw@users.noreply.github.com>
2026-05-13 22:35:20 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.35 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,42 +1,36 @@
 function solution(n, wires) {
-    
-    // 트리 만들기
     const graph = Array.from({length: n+1}, ()=>[]);
     for (const [a, b] of wires) {
         graph[a].push(b);
         graph[b].push(a);
     }
+    // console.log(graph);
     
-    // 깊이 우선 탐색
-    function dfs(node, visited) {
+    let visited = new Set();
+    
+    let cnt = 0;
+    function dfs(node) {
         visited.add(node);
         for (const next of graph[node]) {
-            if (!visited.has(next)) {
-                dfs(next, visited);
+            if(!visited.has(next)) {
+                cnt++;
+                dfs(next);
             }
         }
     }
     
-    let min = n;
-    for (const [a, b] of wires) {
-        // 간선 하나 제거
-        graph[a] = graph[a].filter(v => v !== b);
-        graph[b] = graph[b].filter(v => v !== a);
-        
-        // 트리 순회 (끊어지기 전까지)
-        const visited = new Set();
-        dfs(a, visited);
-        
-        // 트리 크기 구하기
-        const graph1 = visited.size;
-        const graph2 = n - graph1;
-        // 최소값 업데이트
-        min = Math.min(min, Math.abs(graph1 - graph2));
-        
-        // 간선 복귀
+    let answer = n;
+    for (const [a,b] of wires) {
+        graph[a] = graph[a].filter(v => v!==b);
+        graph[b] = graph[b].filter(v => v!==a);
+        cnt = 1;
+        visited = new Set();
+        dfs(a);
+        g2 = n-cnt;
+        answer = Math.min(answer, Math.abs(cnt-g2));
         graph[a].push(b);
         graph[b].push(a);
     }
     
-    return min;
+    return answer;
 }
