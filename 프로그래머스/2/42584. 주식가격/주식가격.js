ec637886a6a015e1a28ddd8d5f786a89f259195d
강은우 <80903685+euoonw@users.noreply.github.com>
2026-05-14 23:51:37 +0900

    [level 2] Title: 주식가격, Time: 11.16 ms, Memory: 45.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,21 +1,16 @@
 function solution(prices) {
-  var answer = [];
-  const stack = []; // 아직 가격 안떨어진 인덱스 대기
-
-  for (let i = 0; i < prices.length; i++) {
-    // 현재 가격 < 스택 맨 위(아직 안떨어진) 인덱스의 가격
-    while (stack.length > 0 && prices[i] < prices[stack[stack.length - 1]]) {
-      const idx = stack.pop(); //
-      answer[idx] = i - idx; // 몇초 버틴건지
+    const result = Array(prices.length-1).fill(1);
+    console.log(result);
+    
+    for (let i=0; i<prices.length-1; i++) {
+      for (let j=i+1; j<prices.length-1; j++) {
+          if (prices[i] <= prices[j]) {
+              result[i]++;
+          } else {
+              break;
+          }
+      }
     }
-    stack.push(i);
-  }
-
-  // 스택에 남아있는거(아직 안떨어진거) 처리
-  while (stack.length > 0) {
-    const idx = stack.pop();
-    answer[idx] = prices.length - 1 - idx;
-  }
-
-  return answer;
+    
+    return [...result, 0];
 }
