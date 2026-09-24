f930436c22466eaafe79235baad9d03ba0b53733
강은우 <80903685+euoonw@users.noreply.github.com>
2026-09-24 21:32:56 +0900

    [level 1] Title: 폰켓몬, Time: 20.79 ms, Memory: 48.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,11 +1,11 @@
 function solution(nums) {
-  var answer = 0;
-  const N_2 = nums.length / 2;
-
-  const pocketmonHash = new Set(nums);
-
-  if (pocketmonHash.size < N_2) answer = pocketmonHash.size;
-  else answer = N_2;
-
-  return answer;
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++ ) {
+        map.set(nums[i], (map.get(nums[i]) || 0) + 1);
+    }
+    const n = nums.length/2;
+    
+    if (n < map.size) return n;
+    else return map.size;
 }
+
