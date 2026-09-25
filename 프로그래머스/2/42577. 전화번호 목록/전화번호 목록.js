0782ee7bc9e7ae10d61618d2d608593aed5334a3
강은우 <80903685+euoonw@users.noreply.github.com>
2026-09-25 23:27:35 +0900

    [level 2] Title: 전화번호 목록, Time: 281.19 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,16 +1,42 @@
 function solution(phone_book) {
     const set = new Set(phone_book);
-    for (let i=0; i<phone_book.length; i++){
-        const phone = phone_book[i];
-        set.delete(phone);
-        let s = ""
-        for (let j=0; j<phone.length; j++){
-            s += phone[j];
-            if (set.has(s)) {
-                return false;
-            }
+    
+    for (let i=0; i<phone_book.length; i++) {
+        let s = '';
+        for (let j=0; j<phone_book[i].length-1; j++) {
+            s += phone_book[i][j];
+            if (set.has(s)) return false;
         }
-        set.add(phone);
     }
+    
     return true;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 이전 답변
+
+// function solution(phone_book) {
+//     const set = new Set(phone_book);
+//     for (let i=0; i<phone_book.length; i++){
+//         const phone = phone_book[i];
+//         let s = ""
+//         for (let j=0; j<phone.length-1; j++){
+//             s += phone[j];
+//             if (set.has(s)) {
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+// }
