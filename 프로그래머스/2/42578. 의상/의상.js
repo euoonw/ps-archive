130fc0a3f78477a93aac02d1ebd2f25f9c4eb9cb
강은우 <80903685+euoonw@users.noreply.github.com>
2026-09-26 16:28:18 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,17 +1,45 @@
 function solution(clothes) {
     const map = new Map();
     for (let i=0; i<clothes.length; i++) {
-        const [value, key] = clothes[i];
-        if (map.has(key)) {
-            map.set(key, map.get(key)+1);
-        } else {
-            map.set(key, 1);
-        }
+        map.set(clothes[i][1], (map.get(clothes[i][1]) || 0) +1);
     }
-    console.log(map)
-    let answer = 1;
+    let result = 1;
     for (const [key, value] of map) {
-        answer *= (value + 1);
+        result *= (value+1)
     }
-    return answer - 1;
+    return result-1;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function solution(clothes) {
+//     const map = new Map();
+//     for (let i=0; i<clothes.length; i++) {
+//         const [value, key] = clothes[i];
+//         if (map.has(key)) {
+//             map.set(key, map.get(key)+1);
+//         } else {
+//             map.set(key, 1);
+//         }
+//     }
+//     console.log(map)
+//     let answer = 1;
+//     for (const [key, value] of map) {
+//         answer *= (value + 1);
+//     }
+//     return answer - 1;
+// }
